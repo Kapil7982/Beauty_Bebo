1d2f365c8730697f0fc207bb193f5bd9e0d85a61
kunal chitatwar <kunals14chitatwar@gmail.com>
2022-06-18 14:35:00 +0530

    homepgae updated

---
+++ homepage/beutybebo/compo/footer.js
@@ -0,0 +1,79 @@
+function footer(){
+    return `
+    <div id="forcontQU">
+    <div>
+        <h3>
+            CONTACT INFO
+        </h3>
+        <p style="color:#dd0285;">- - - - - - -</p>
+        <P style="margin-top:20px; margin-bottom: 20px;">(+91)8120337567</P>
+        <p>support@beautybebo.com</p>
+        <p>sales@beautybebo.com</p>
+        <p style="margin-top:20px; margin-bottom: 20px;">Open time 10:00AM - 7:00PM</p>
+        <div>
+            <img src="" alt="">
+        </div>
+    </div>
+    <div>
+        <h3>
+            QUICK LINKS
+        </h3>
+        <p style="color:#dd0285;">- - - - - - -</p>
+        <ul class="forfoolist" >
+            <li>About us</li>
+            <li>contact Us</li>
+            <li>Terms & Conditions</li>
+            <li>Pricacy Policy</li>
+            <li>Return and Refund Policy</li>
+            <li>Shipping Policy</li>
+        </ul>
+    </div>
+    <div>
+        <h3>
+            CUSTOMER INFO
+        </h3>
+        <p style="color:#dd0285;">- - - - - - -</p>
+        <ol class="forfoolist" >
+            <li>My Account</li>
+            <li>Track your Order</li>
+            <li>Order Returns</li>
+            <li>Wishlist</li>
+            <li>News & Events</li>
+            <li>FAQ</li>
+        </ol>
+    </div>
+    <div>
+        <h3>
+            CATEGORIES
+        </h3>
+        <p style="color:#dd0285;">- - - - - - -</p>
+        <ol class="forfoolist">
+            <li>Makeup</li>
+            <li>Skin</li>
+            <li>Hair</li>
+            <li>Personal Care</li>
+            <li>Mom & Baby Care</li>
+            <li>Fragrance</li>
+            <li>Ayurveda</li>
+        </ol>
+    </div>
+</div>
+<div style="width:1200px; background-color: white; height: .5px; margin-left: auto; margin-right: auto; margin-bottom: 7px;"></div>
+<div style="display:flex; justify-content: space-between; width: 1200px; margin: auto;">
+    <div>
+        <img src="https://www.beautybebo.com/pub/media/wysiwyg/payment.png" alt="">
+    </div>
+    <div id="SignUPpartfooter" style="display:flex; align-items: center;">
+        <h3>Signup For Newsletter</h3>
+        &nbsp;  &nbsp;  &nbsp;  &nbsp;
+        <input type="text" placeholder="Signup for Newsletter">
+        <button>Subscribe</button>
+    </div>
+</div>
+<div id="ARR">
+   <p>© 2020 All Rights Reserved.</p>
+</div>
+</div>`
+}
+
+export default footer;
